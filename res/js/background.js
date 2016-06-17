c6ee151aae1707740477dec6bcb568dc1391f40b
unclehking <513935463@qq.com>
2016-06-17 12:57:09 +0800

    newsomething

--- res/js/background.js
+++ res/js/background.js
@@ -1,5 +1,5 @@
 ﻿/*HKing 2016-03-10*/
-const version = "1.0";
+const version = "1.01";
 const url="d3d3LnpoaWh1LmNvbQ==";
 chrome.browserAction.onClicked.addListener((tab) =>{
 	chrome.tabs.getAllInWindow(null, fuckBat = tabs =>{
@@ -14,13 +14,39 @@ chrome.browserAction.onClicked.addListener((tab) =>{
 	});
 });
 
+
+
 var xhr = new XMLHttpRequest();
 xhr.open ('GET','https://raw.githubusercontent.com/unclehking/zhihu-material-design-theme/master/version.json');
 xhr.send();
 xhr.onreadystatechange = function (){
     if ( xhr.readyState == 4 && xhr.status == 200 ){
-		if(xhr.responseText == version){
+		if(JSON.parse(xhr.responseText).version == version){
 			console.log(xhr.responseText);
+		}else{
+			//
 		}
     }
 };　
+
+function notify(){
+	var opt = {
+        type: "list",
+        title: "知乎MD主题扩展有更新！",
+        message: "msg",
+        iconUrl: "res/pic/icon_128.png",
+        items: [
+			{ title: "1.", message: "点击进入下载最新版本！"}
+		]
+	}
+  	chrome.notifications.create('',opt,function(id){
+
+	});
+
+}
+
+notify();
+
+chrome.notifications.onClicked.addListener(function(id){
+	window.open("https://github.com/unclehking/zhihu-material-design-theme");
+});
